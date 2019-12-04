9a431bc6c682053ed4b165961757fc73ceff8708
Byeong Gyu Choi <bisschoi9541@gmail.com>
2019-12-04 20:56:53 +0900

    Lint

--- src/sockethandler.js
+++ src/sockethandler.js
@@ -1,15 +1,15 @@
 const request = require('request-promise-native');
 
 function setHandler(io, port) {
-  io.on('connection', socket => {
-    socket.on('message', async jsonData => {
+  io.on('connection', (socket) => {
+    socket.on('message', async (jsonData) => {
       // Client socket.send 요청 (인자 : JSON { sender, text })
-      jsonData = JSON.parse(jsonData);
+      const data = JSON.parse(jsonData);
 
       const options = {
         method: 'get',
         url: `http://localhost:${port}/translate`,
-        qs: { text: jsonData.text },
+        qs: { text: data.text },
         json: true,
       };
 
@@ -17,22 +17,22 @@ function setHandler(io, port) {
       try {
         langCode = await request(options); // GET /translate
         langCode = langCode.result;
-        jsonData.source = langCode;
+        data.source = langCode;
       } catch (e) {
-        jsonData.source = null;
+        data.source = null;
       }
-      socket.broadcast.send(JSON.stringify(jsonData));
+      socket.broadcast.send(JSON.stringify(data));
       // 새로온 메세지 모두에게 전달(인자 : JSON { sender, text, source })
     });
 
     socket.on('disconnect', () => {
       io.send(
-        JSON.stringify({ sender: null, text: 'Someone Left the Chat Room' })
+        JSON.stringify({ sender: null, text: 'Someone Left the Chat Room' }),
       );
     });
 
     io.send(
-      JSON.stringify({ sender: null, text: 'New Person Entered the Chat Room' })
+      JSON.stringify({ sender: null, text: 'New Person Entered the Chat Room' }),
     );
   });
 }
